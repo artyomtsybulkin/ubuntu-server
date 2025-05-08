0c1184e6d0239646cc95eaff5931ad98b0354489
Artyom Tsybulkin <artyomtsybulkin@outlook.com>
2025-05-08 13:16:18 +0300

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Installation tips
+# 1. Use LTS version of Ubuntu
+# 2. Choose langinguage and keyboard layout: English (US)
+# 3. Type of installation: Ubuntu Server (minimized)
+# 4. Install OpenSSH server - Enable
+# 5. Disable LVM
+# 6. Change filesystem to XFS instead of EXT4
+
