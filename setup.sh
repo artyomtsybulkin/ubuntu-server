3d878780af66cf0ee89a8c9ac59ed44ac0027998
Artyom Tsybulkin <artyomtsybulkin@outlook.com>
2025-05-09 16:06:25 +0300

    Installing, configuring #1

--- setup.sh
+++ setup.sh
@@ -6,3 +6,27 @@
 # 5. Disable LVM
 # 6. Change filesystem to XFS instead of EXT4
 
+# Post-installation steps
+apt update -y && apt upgrade -y
+
+# Fix this issue:
+# debconf: unable to initialize frontend: Dialog
+# debconf: (No usable dialog-like program is installed, so the dialog based frontend cannot be used. at /usr/share/perl5/Debconf/FrontEnd/Dialog.pm line 79.)
+# debconf: falling back to frontend: Readline
+apt install -y dialog
+
+# Install base packages
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    software-properties-common \
+    net-tools \
+    htop \
+    git \
+    vim \
+    wget \
+    unzip \
+    zip
